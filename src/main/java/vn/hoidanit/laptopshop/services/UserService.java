cd18c6176fa56479cd651993c01ba9492c15a908
Viet Hoang <gvhoang30112004@gmail.com>
2024-06-02 19:04:07 +0700

    tạo cấu trúc thư mục

--- src/main/java/vn/hoidanit/laptopshop/services/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/services/UserService.java
@@ -3,6 +3,9 @@ package vn.hoidanit.laptopshop.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyService {
+public class UserService {
 
+  public String handleHello() {
+    return "Hello from Service";
+  }
 }
